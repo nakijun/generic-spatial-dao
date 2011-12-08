ea624e5101905b2f2041272f119c07b0c5e3088e
joaosavio <joaosavio@40a37862-23c8-7f89-a8b1-03332071242e>
2011-12-08 17:24:17 +0000

    added test

---
+++ generic-spatial-dao/src/test/java/org/genericspatialdao/example/ExampleTest.java
@@ -0,0 +1,60 @@
+package org.genericspatialdao.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.persistence.EntityManager;
+
+import org.genericspatialdao.dao.DAO;
+import org.genericspatialdao.dao.DAOHelper;
+import org.genericspatialdao.dao.GenericDAO;
+import org.genericspatialdao.example.vo.Department;
+import org.genericspatialdao.example.vo.Employee;
+import org.genericspatialdao.utils.DAOUtils;
+import org.genericspatialdao.utils.DataCreator;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void oneToManyTest() {
+		// setup
+		DAO<Department> testD = new GenericDAO<Department>(Department.class);
+
+		DAO<Employee> testE = new GenericDAO<Employee>(Employee.class);
+
+		Department d = DataCreator.createDepartment();
+		testD.persist(d);
+		assertEquals(1, testD.findAll().size());
+
+		Employee e1 = DataCreator.createEmployee();
+		Employee e2 = DataCreator.createEmployee();
+		testE.persist(e1);
+		testE.persist(e2);
+		assertEquals(2, testE.findAll().size());
+
+		// action
+		d.getEmployees().add(e1);
+		d.getEmployees().add(e2);
+		e1.setDepartment(d);
+		e2.setDepartment(d);
+		testD.merge(d);
+		// em.merge(employee1);
+		// em.merge(employee2);
+		// em.flush();
+
+		// em.refresh(department);
+		// department = em.find(Department.class, d.getId());
+		assertTrue(d.getEmployees().contains(e1));
+		assertTrue(d.getEmployees().contains(e2));
+		assertEquals(d, e1.getDepartment());
+		assertEquals(d, e2.getDepartment());
+
+		testE.remove(e2);
+		testE.remove(e1);
+		testD.remove(d);
+
+		DAOUtils.close(testE, testD);
+	}
+
+}
