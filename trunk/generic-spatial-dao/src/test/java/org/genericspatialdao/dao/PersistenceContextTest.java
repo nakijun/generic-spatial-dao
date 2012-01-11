30e4ad4ea235252f973de87e16f6d80e24e70b1e
joaosavio <joaosavio@40a37862-23c8-7f89-a8b1-03332071242e>
2012-01-11 16:33:55 +0000

    fixed javadoc warnings, added tests

--- trunk/generic-spatial-dao/src/test/java/org/genericspatialdao/dao/PersistenceContextTest.java
+++ trunk/generic-spatial-dao/src/test/java/org/genericspatialdao/dao/PersistenceContextTest.java
@@ -1,14 +1,52 @@
 package org.genericspatialdao.dao;
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.genericspatialdao.exception.DAOException;
 import org.junit.Test;
 
 public class PersistenceContextTest {
 
 	@Test
-	public void simpleTest() {
+	public void loadAndUnloadPersistenceUnitsTest() {
+		System.out.println("loadAndUnloadPersistenceUnitsTest");
 		PersistenceContext.loadPersistenceUnit("default");
 		PersistenceContext.loadPersistenceUnit("test");
 		PersistenceContext.loadPersistenceUnit("default");
+	}
+
+	@Test
+	public void defaultLoadOnInitTest() {
+		assertTrue(PersistenceContext.isPersistenceUnitLoaded());
+	}
+
+	@Test
+	public void closeFactoryTest() {
+		System.out.println("closeFactoryTest");
+		PersistenceContext.closeFactory();
+		assertFalse(PersistenceContext.isPersistenceUnitLoaded());
+		PersistenceContext.loadPersistenceUnit("default");
+	}
+
+	@Test
+	public void closeEntityManagerTest() {
+		System.out.println("closeEntityManagerTest");
+		PersistenceContext.close();
+		assertNotNull(PersistenceContext.getEntityManager());
+	}
+
+	@Test(expected = DAOException.class)
+	public void getEntityManagerSessionAndFactoryClosedTest() {
+		System.out.println("getEntityManagerSessionAndFactoryClosedTest");
+		try {
+			PersistenceContext.close();
+			PersistenceContext.closeFactory();
+			PersistenceContext.getEntityManager();
+		} finally {
+			PersistenceContext.loadPersistenceUnit("default");
+		}
 	}
 
 }
