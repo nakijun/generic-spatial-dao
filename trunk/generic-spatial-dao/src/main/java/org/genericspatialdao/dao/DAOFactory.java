b552d2673f1786db76910bc33c00b7a576724037
joaosavio <joaosavio@40a37862-23c8-7f89-a8b1-03332071242e>
2012-03-29 19:53:00 +0000

    new release

--- trunk/generic-spatial-dao/src/main/java/org/genericspatialdao/dao/DAOFactory.java
+++ trunk/generic-spatial-dao/src/main/java/org/genericspatialdao/dao/DAOFactory.java
@@ -1,11 +1,10 @@
 package org.genericspatialdao.dao;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.genericspatialdao.utils.PropertiesUtils;
 
 public class DAOFactory {
 
+	private static final String CREATING_DAO_FOR_CLASS = "Creating DAO for class ";
 	private static final Logger LOG = Logger.getLogger(DAOFactory.class);
 
 	private DAOFactory() {
@@ -18,12 +17,9 @@ public class DAOFactory {
 	 * @return a DAO implementation
 	 */
 	public static <T> DAO<T> getDAO(Class<T> clazz) {
-		String defaultPersistenceUnit = PropertiesUtils
-				.getString("persistenceunit.default");
-		if (StringUtils.isBlank(defaultPersistenceUnit)) {
-			defaultPersistenceUnit = "default";
-		}
-		return getDAO(clazz, defaultPersistenceUnit);
+		LOG.info(CREATING_DAO_FOR_CLASS + clazz.getName()
+				+ " using default persistence unit");
+		return new GenericSpatialDAO<T>(clazz);
 	}
 
 	/**
@@ -32,7 +28,7 @@ public class DAOFactory {
 	 * @return a DAO implementation
 	 */
 	public static <T> DAO<T> getDAO(Class<T> clazz, String persistenceUnitName) {
-		LOG.info("Creating DAO for class " + clazz.getName()
+		LOG.info(CREATING_DAO_FOR_CLASS + clazz.getName()
 				+ " using persistence unit: " + persistenceUnitName);
 		return new GenericSpatialDAO<T>(clazz, persistenceUnitName);
 	}
--- trunk/generic-spatial-dao/src/main/java/org/genericspatialdao/utils/ConstantsUtils.java
+++ trunk/generic-spatial-dao/src/main/java/org/genericspatialdao/utils/ConstantsUtils.java
@@ -1,5 +1,7 @@
 package org.genericspatialdao.utils;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ConstantsUtils {
 
 	public static final String SPACE = " ";
@@ -13,7 +15,60 @@ public class ConstantsUtils {
 	public static final String SLASH = "/";
 	public static final String REVERSE_SLASH = "\\";
 	public static final String LEFT_BRACE = "{";
-	public static final String RIGHT_BRACE = "}"; 
+	public static final String RIGHT_BRACE = "}";
 	public static final String LEFT_PARENTHESES = "(";
 	public static final String RIGHT_PARENTHESES = ")";
+
+	// root default
+	public static final String DEFAULT_PERSISTENCE_UNIT = "default";
+	// default by user
+	public static final String DEFAULT_APPLICATION_PERSISTENCE_UNIT;
+	public static final boolean AUTO_BEGIN_TRANSACTION;
+	public static final boolean AUTO_COMMIT;
+	public static final boolean AUTO_ROLLBACK;
+
+	// properties
+	private static final String PERSISTENCEUNIT_DEFAULT_PROPERTY = "persistenceunit.default";
+	private static final String BEGINTRANSACTION_AUTO_PROPERTY = "begintransaction.auto";
+	private static final String COMMIT_AUTO_PROPERTY = "commit.auto";
+	private static final String ROLLBACK_AUTO_PROPERTY = "rollback.auto";
+
+	static {
+		String defaultPersistenceUnit = PropertiesUtils
+				.getString(PERSISTENCEUNIT_DEFAULT_PROPERTY);
+		if (StringUtils.isBlank(defaultPersistenceUnit)) {
+			defaultPersistenceUnit = DEFAULT_PERSISTENCE_UNIT;
+		}
+		DEFAULT_APPLICATION_PERSISTENCE_UNIT = defaultPersistenceUnit;
+
+		String autoBeginTransactionString = PropertiesUtils
+				.getString(BEGINTRANSACTION_AUTO_PROPERTY);
+		if (StringUtils.isBlank(autoBeginTransactionString)
+				|| StringUtils.equalsIgnoreCase(autoBeginTransactionString,
+						Boolean.TRUE.toString())) {
+			AUTO_BEGIN_TRANSACTION = true;
+		} else {
+			AUTO_BEGIN_TRANSACTION = false;
+		}
+
+		String autoCommitString = PropertiesUtils
+				.getString(COMMIT_AUTO_PROPERTY);
+		if (StringUtils.isBlank(autoCommitString)
+				|| StringUtils.equalsIgnoreCase(autoCommitString,
+						Boolean.TRUE.toString())) {
+			AUTO_COMMIT = true;
+		} else {
+			AUTO_COMMIT = false;
+		}
+
+		String autoRollbackString = PropertiesUtils
+				.getString(ROLLBACK_AUTO_PROPERTY);
+		if (StringUtils.isBlank(autoRollbackString)
+				|| StringUtils.equalsIgnoreCase(autoRollbackString,
+						Boolean.TRUE.toString())) {
+			AUTO_ROLLBACK = true;
+		} else {
+			AUTO_ROLLBACK = false;
+		}
+	}
 }
