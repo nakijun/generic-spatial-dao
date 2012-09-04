c6124f0fd48b9cac9d0fb11e01851b44705c6749
joaosavio <joaosavio@40a37862-23c8-7f89-a8b1-03332071242e>
2012-09-04 01:22:31 +0000

    new version

---
+++ trunk/generic-spatial-dao/src/test/resources/sql/createDBs.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE genericspatialdao1 WITH TEMPLATE template_postgis;
+CREATE DATABASE genericspatialdao2 WITH TEMPLATE template_postgis;
+CREATE DATABASE genericspatialdao3 WITH TEMPLATE template_postgis;
+CREATE DATABASE genericspatialdao4 WITH TEMPLATE template_postgis;
---
+++ trunk/generic-spatial-dao/src/test/resources/sql/dropDBs.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS genericspatialdao1;
+DROP DATABASE IF EXISTS genericspatialdao2;
+DROP DATABASE IF EXISTS genericspatialdao3;
+DROP DATABASE IF EXISTS genericspatialdao4;
