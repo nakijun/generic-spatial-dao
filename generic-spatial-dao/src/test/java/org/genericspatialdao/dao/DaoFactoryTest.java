869fea2575246f79d96531941b0ec6f0f8b17957
joaosavio <joaosavio@40a37862-23c8-7f89-a8b1-03332071242e>
2012-12-17 18:11:47 +0000

    stable now

---
+++ generic-spatial-dao/src/test/java/org/genericspatialdao/dao/DaoFactoryTest.java
@@ -0,0 +1,41 @@
+package org.genericspatialdao.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.genericspatialdao.configuration.DaoConfiguration;
+import org.genericspatialdao.dao.impl.DaoFactory;
+import org.genericspatialdao.data.SpatialTestVO;
+import org.genericspatialdao.data.TestVO;
+import org.genericspatialdao.util.TestUtils;
+import org.genericspatialdao.util.TestUtils.DB;
+import org.junit.Test;
+
+public class DaoFactoryTest {
+
+	@Test
+	public void test() {
+		Dao<TestVO> testDAO = DaoFactory.getDAO(
+				TestVO.class,
+				new DaoConfiguration(DB.DB_1.toString(), TestUtils
+						.buildPropertiesMap(DB.DB_1)));
+		assertEquals(true, testDAO instanceof Dao);
+	}
+
+	@Test
+	public void closeTest() {
+		Dao<TestVO> testDAO = DaoFactory.getDAO(
+				TestVO.class,
+				new DaoConfiguration(DB.DB_1.toString(), TestUtils
+						.buildPropertiesMap(DB.DB_1)));
+		Dao<SpatialTestVO> testDAO2 = DaoFactory.getDAO(
+				SpatialTestVO.class,
+				new DaoConfiguration(DB.DB_1.toString(), TestUtils
+						.buildPropertiesMap(DB.DB_1)));
+		DaoFactory.close(testDAO, testDAO2);
+	}
+
+	@Test
+	public void closeAllTest() {
+		DaoFactory.closeAll();
+	}
+}
